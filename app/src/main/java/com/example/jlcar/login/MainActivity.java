27b4b58452bdc8f133830e7c4a16bed5e51b8b86
pancho <pancho1061@gmail.com>
2018-06-19 16:44:47 -0400

    probando git

--- app/src/main/java/com/example/jlcar/login/MainActivity.java
+++ app/src/main/java/com/example/jlcar/login/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jlcar.login;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.example.jlcar.login.entidades.Usuario;
-
 import OpenHelper.SQLite_OpenHelper;
 
 public class MainActivity extends AppCompatActivity {
     TextView lblRegistrar;
     Button btnIngresar;
+    private Cursor fila;
 
     SQLite_OpenHelper helper = new SQLite_OpenHelper(this, "BD1", null, 1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
+                SQLiteDatabase bd = helper.getWritableDatabase();
 
                 EditText txtUsu = (EditText)findViewById(R.id.txtUsuario);
                 EditText txtPass = (EditText)findViewById(R.id.txtpass);
@@ -47,14 +49,15 @@ public class MainActivity extends AppCompatActivity {
                 String usu = txtUsu.getText().toString();
                 String pass = txtPass.getText().toString();
 
+                fila = bd.rawQuery("select Usuario, Password, id_us from usuarios where Usuario='"+usu+"'and Password='"+pass+"'",null);
 
-                Cursor cursor = helper.ConsultarUsupas(usu, pass);
+                //Cursor cursor = helper.ConsultarUsupas(usu, pass);
 
-                if(cursor.getCount()>0)
-                {
-                    Intent i = new Intent(getApplicationContext(), Controlador_perfil.class);
-                    Usuario u = new Usuario();
-                    i.putExtra( "id_us", u.getId_us());
+                if(fila.moveToFirst()==true){
+
+                    int id=fila.getInt(2);
+                    Intent i = new Intent(getApplicationContext(),Controlador_perfil.class);
+                    i.putExtra( "identificador_us", id);
                     startActivity(i);
                 }
                 else
@@ -65,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 txtPass.setText("");
                 txtUsu.findFocus();
 
-
+    //probando github
             }
         });
     }
